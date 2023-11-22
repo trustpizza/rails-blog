c03d8e6e80d40366a3442329323298cc0ea215c1
Axel <maxelolsson@gmail.com>
2023-11-22 08:46:56 -0500

    Carousel controller now works appropriately, needs styling, etc

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -1,9 +1,54 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = [ "slide" ]
+  static targets = [ "slide", "button" ]
+  static values = {
+    index: Number,
+    default: 0
+  }
+  
+  initialize () {
+    this.index = this.indexValue
+    // setInterval(() => {
+    //     // this.nextSlide
+    // }, 1000 * 1.5)
+  }
+  
+  showCurrentSlide() {
+    this.slideTargets.forEach((slide, i) => {
+      slide.classList.toggle("hidden", i !== this.index)
+    });
     
-    click() {
-        console.log(this.slideTarget)
+    this.buttonTargets.forEach((button, i) => {
+      // button.toggleAttribute("active")
+      button.classList.toggle("active", i !== this.index)
+    })
+    
+  }
+  
+  next() {
+    if ((this.index + 1) > (this.slideTargets.length - 1)) {
+      this.index = 0
+    } else {
+      this.index++
     }
+    
+    this.showCurrentSlide()
+  }
+  
+  previous() {
+    if ((this.index -1 ) < 0) {
+      this.index = this.slideTargets.length - 1
+    } else {
+      this.index--
+    }
+    
+    this.showCurrentSlide()
+  }
+  
+  click() {
+    this.next()
+    console.log(this.index)
+    // console.log(this.slideTarget)    
+  }
 }
