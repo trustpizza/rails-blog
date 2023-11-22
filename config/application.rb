27453a525268ad7c0051e1e6ab6887e1ae5784c6
Axel <maxelolsson@gmail.com>
2023-11-22 13:12:58 -0500

    remove moon.ttf and add fonts/moon.ttf

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Blog
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
