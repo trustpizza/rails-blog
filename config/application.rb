57925502b311be2c22a4de660b1a03382eb383b8
Axel <maxelolsson@gmail.com>
2023-11-22 12:59:14 -0500

    Creating pause for figuring out fonts

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Blog
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
     # Configuration for the application, engines, and railties goes here.
     #
