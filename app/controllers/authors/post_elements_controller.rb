e794c7ced2dfb07e75ec3a8ee051f9b9e1e216ca
Axel <maxelolsson@gmail.com>
2023-12-04 15:28:22 -0500

    remove json passes

--- app/controllers/authors/post_elements_controller.rb
+++ app/controllers/authors/post_elements_controller.rb
@@ -9,7 +9,7 @@ module Authors
 
       respond_to do |format|
         if @post_element.save
-          format.html { redirect_to edit_post_path(@post)}
+          format.html { redirect_to edit_post_path(@post) }
         else
           format.html { redirect_to edit_post_path(@post), notice: @post_element.errors.full_messages.join(". ") << "." }
         end
--- app/controllers/authors/posts_controller.rb
+++ app/controllers/authors/posts_controller.rb
@@ -24,10 +24,8 @@ module Authors
       respond_to do |format|
         if @post.save
           format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-          format.json { render :show, status: :created, location: @post }
         else
           format.html { render :new, status: :unprocessable_entity }
-          format.json { render json: @post.errors, status: :unprocessable_entity }
         end
       end
     end
@@ -37,10 +35,8 @@ module Authors
       respond_to do |format|
         if @post.update(post_params)
           format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
-          format.json { render :show, status: :ok, location: @post }
         else
           format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: @post.errors, status: :unprocessable_entity }
         end
       end
     end
@@ -51,7 +47,6 @@ module Authors
 
       respond_to do |format|
         format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
-        format.json { head :no_content }
       end
     end
 
