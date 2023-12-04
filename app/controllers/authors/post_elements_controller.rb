c720c2c222cbf4954d86594e94e6c4f3e2ca1acf
Axel <maxelolsson@gmail.com>
2023-12-04 16:00:52 -0500

    Prep form, needs turbostream

--- app/controllers/authors/post_elements_controller.rb
+++ app/controllers/authors/post_elements_controller.rb
@@ -22,7 +22,7 @@ module Authors
     def update
       respond_to do |format|
         if @post_element.update(post_element_params)
-          format.html { redirect_to post_element_url(@post_element), notice: "Post element was successfully updated." }
+          format.html { redirect_to edit_post_path(@post_element.post), notice: "yay" }
         else
           format.html { render :edit, status: :unprocessable_entity }
         end
