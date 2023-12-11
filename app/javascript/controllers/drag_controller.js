c348d9d57ebb824b4710794ba102970bcede6f48
Axel <maxelolsson@gmail.com>
2023-12-11 13:14:22 -0500

    add drag controller.js

---
+++ app/javascript/controllers/drag_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+import Sortable from "sortablejs"
+
+export default class extends Controller {
+  connect() {
+    this.sortable = Sortable.create(this.element, {
+      onEnd: this.end.bind(this)
+    });
+  }
+  end(event) {
+    console.log(event)
+  }
+}
