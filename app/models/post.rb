86a528e55fa9e5ad562806e5258e50251dd8b7ff
Axel <maxelolsson@gmail.com>
2023-12-13 22:34:37 -0500

    Save

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author
 
-  has_many :post_elements, -> { order(position: :asc) }
+  has_many :post_elements, -> { order(position: :asc) }, dependent: :destroy
   has_one_attached :header_image
 end
