dd9d5336f11e157d8f69543f6294fa0594c392cb
Axel <maxelolsson@gmail.com>
2023-12-14 09:31:40 -0500

    Add some validations for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,7 @@ class Post < ApplicationRecord
 
   has_many :post_elements, -> { order(position: :asc) }, dependent: :destroy
   has_one_attached :header_image
+
+  validates :title, presence: true, length: {minimum: 10}
+  validates :description, presence: true, length: { in: 50..250 }
 end
