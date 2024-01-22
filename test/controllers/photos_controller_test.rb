ba1ba6cb90087663cf53f56755bb15b8e4a5092b
Axel <maxelolsson@gmail.com>
2024-01-22 09:56:39 -0500

    Add photos controller correctly

--- test/controllers/photos_controller_test.rb
+++ test/controllers/photos_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class PhotosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get photos_index_url
+    assert_response :success
+  end
 end
