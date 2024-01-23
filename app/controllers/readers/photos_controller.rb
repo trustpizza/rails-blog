e08d0decb8843e0bac03071137a1ab3310559781
Axel <maxelolsson@gmail.com>
2024-01-23 12:41:06 -0500

    Prep for javascript photos controller

--- app/controllers/readers/photos_controller.rb
+++ app/controllers/readers/photos_controller.rb
@@ -1,7 +1,20 @@
 module Readers 
   class PhotosController < ReadersController
     def index
+      @photos = get_photos(Post.all)
+      puts @photos
+    end
+
+    private
+
+    def get_photos(posts)
+      photos = []
+
+      posts.each do |post|
+        photos << post.header_image
+      end
       
+      photos
     end
   end
 end
