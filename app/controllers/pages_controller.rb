b4016eea7a676df94ba84ccdb8483ab7c7c50845
Axel <maxelolsson@gmail.com>
2024-05-14 09:06:52 -0400

    Get basics w/o css of Admins only pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  before_action :authenticate_admin!, except: %i[ home ] 
+  def home
+  end
+
+  def admin
+    @users = User.all
+    @posts = Post.all
+  end
+
+  private
+
+  def authenticate_admin!
+    unless current_user && current_user.is_admin?
+      redirect_to new_user_session_path
+    end
+  end
+
+end
