e8ce6b2f09a4a8a2f8b92e760bf6f94d08017050
Axel <maxelolsson@gmail.com>
2024-06-19 22:00:39 -0400

    Fix looks to get images working

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,17 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
     './app/assets/images/*.svg'
-  ] 
+  ],
+  theme: {
+    extend: {
+      maxHeight: {
+        "128": "32rem",
+        "80vh": "80vh"
+      },
+      maxWidth: {
+        "128": "32rem",
+        "80vw": "80vw"
+      }
+    }
+  }
 }
